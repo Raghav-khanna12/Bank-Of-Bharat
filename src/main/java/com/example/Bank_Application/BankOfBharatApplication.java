0c2f53dd91c20716216c5a4914759e975cb428ec
Raghav-khanna12 <raghavk2509@gmail.com>
2024-05-30 19:12:34 +0530

    Added dependecies in the POM.XML fi;e

---
+++ src/main/java/com/example/Bank_Application/BankOfBharatApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bank_Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOfBharatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankOfBharatApplication.class, args);
+	}
+
+}
